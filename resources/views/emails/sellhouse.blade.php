e3be486e4dae983bfbbbbdddf85049806e5bba4b
Roberto Heredia <raherediag@gmail.com>
2016-06-18 12:07:44 -0400

    testing schedule for sendmail;

--- resources/views/emails/sellhouse.blade.php
+++ resources/views/emails/sellhouse.blade.php
@@ -1,15 +1,15 @@
-***Se vende Apartamento en el Distrito "Jose Martí" Santiago de Cuba
-    Bloque F-51 Apto 6 (3er Piso)
-    2 cuartos con la división
-    Cocina Enchapada, Baño Azulejeado
-    Sala Comedor , Ampliación del balcón con Ventanas de Hierro y Cristal
-    Localizada a una cuadra de la Parada de la 24 y las camionetas en la
-    Avenida de las Américas.
-    Privacidad, no tiene edificios al frente ni detrás
-    Al frente Bodega,Carnicería y Círculo Infantil,
-    al lado Area Deportiva (Cancha de Basket y Voleibol)
-    Se anuncia Por 5500 CUC
-
-****Teléfonos
-Cel 54398556  Yelene
-Cel 53687089  Roberto
+***Se vende Apartamento en el Distrito "Jose Martí" Santiago de Cuba<br>
+    Bloque F-51 Apto 6 (3er Piso)<br>
+    2 cuartos con la división<br>
+    Cocina Enchapada, Baño Azulejeado<br>
+    Sala Comedor , Ampliación del balcón con Ventanas de Hierro y Cristal<br>
+    Localizada a una cuadra de la Parada de la 24 y las camionetas en la<br>
+    Avenida de las Américas.<br>
+    Privacidad, no tiene edificios al frente ni detrás<br>
+    Al frente Bodega,Carnicería y Círculo Infantil,<br>
+    al lado Area Deportiva (Cancha de Basket y Voleibol)<br>
+    Se anuncia Por 5500 CUC<br>
+<br>
+****Teléfonos<br>
+    Cel 54398556  Yelene<br>
+    Cel 53687089  Roberto<br>

2e29a50b669c3c5d2a63eb6bb0e942f75fd8a7a7
Roberto Heredia <raherediag@gmail.com>
2016-06-18 11:36:33 -0400

    Prepraring schedule for sendmail;

---
+++ app/Console/Commands/mySendMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class mySendMail extends Command {
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mySendMail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send Mails To ComercioOnline and Las-Avispas';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        Mail::send('emails.sellhouse', [], function($message) {
+            $message->to('raherediag@gmail.com', 'Roberto A. Heredia')->subject('Test');
+        });
+
+        $this->info('Email was sent.');
+    }
+
+}
---
+++ resources/views/emails/sellhouse.blade.php
@@ -0,0 +1,15 @@
+***Se vende Apartamento en el Distrito "Jose Martí" Santiago de Cuba
+    Bloque F-51 Apto 6 (3er Piso)
+    2 cuartos con la división
+    Cocina Enchapada, Baño Azulejeado
+    Sala Comedor , Ampliación del balcón con Ventanas de Hierro y Cristal
+    Localizada a una cuadra de la Parada de la 24 y las camionetas en la
+    Avenida de las Américas.
+    Privacidad, no tiene edificios al frente ni detrás
+    Al frente Bodega,Carnicería y Círculo Infantil,
+    al lado Area Deportiva (Cancha de Basket y Voleibol)
+    Se anuncia Por 5500 CUC
+
+****Teléfonos
+Cel 54398556  Yelene
+Cel 53687089  Roberto

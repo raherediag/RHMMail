eaae73bc5a767fde670a4bdba6fb8cb4f6369284
Roberto Heredia <raherediag@gmail.com>
2018-01-08 23:40:52 -0500

    Added reello template and updated.

--- app/Console/Commands/mySendMail.php
+++ app/Console/Commands/mySendMail.php
@@ -32,7 +32,7 @@ class mySendMail extends Command
             $message->to('comercioonline@googlegroups.com', 'comercioonline');
             $message->to('las-avispas@googlegroups.com', 'las-avispas');
             // $message->cc('raherediag@gmail.com', 'Roberto A. Heredia');
-            $message->subject('Se vende Split ROYAL 1Tn 220v a 600 CUC');
+            $message->subject('Se vende Split ROYAL 1Tn 220v a 600 CUC en la Habana');
         });
         \Log::info('Email Split ROYAL was sent.');
 
@@ -40,7 +40,7 @@ class mySendMail extends Command
             $message->to('comercioonline@googlegroups.com', 'comercioonline');
             $message->to('las-avispas@googlegroups.com', 'las-avispas');
             // $message->cc('raherediag@gmail.com', 'Roberto A. Heredia');
-            $message->subject('Se vende Lavadora LG 8.6 Kg a 500 CUC');
+            $message->subject('Se vende Lavadora LG 8.6 Kg a 500 CUC en la Habana en la Habana');
         });
         \Log::info('Email Lavadora LG 8.6 Kg was sent.');
 
@@ -48,7 +48,7 @@ class mySendMail extends Command
             $message->to('comercioonline@googlegroups.com', 'comercioonline');
             $message->to('las-avispas@googlegroups.com', 'las-avispas');
             // $message->cc('raherediag@gmail.com', 'Roberto A. Heredia');
-            $message->subject('Se vende Cocina ROYAL Gas 4Q con Horno a 280 CUC');
+            $message->subject('Se vende Cocina ROYAL Gas 4Q con Horno a 280 CUC en la Habana');
         });
         \Log::info('Email Cocina ROYAL Gas was sent.');
 
@@ -56,7 +56,7 @@ class mySendMail extends Command
             $message->to('comercioonline@googlegroups.com', 'comercioonline');
             $message->to('las-avispas@googlegroups.com', 'las-avispas');
             // $message->cc('raherediag@gmail.com', 'Roberto A. Heredia');
-            $message->subject('Se vende Lijadoras 220w STANLEY a 100 CUC');
+            $message->subject('Se vende Lijadoras 220w STANLEY a 100 CUC en la Habana');
         });
         \Log::info('Email Lijadoras 220w STANLEY was sent.');
         
@@ -64,9 +64,17 @@ class mySendMail extends Command
             $message->to('comercioonline@googlegroups.com', 'comercioonline');
             $message->to('las-avispas@googlegroups.com', 'las-avispas');
             // $message->cc('raherediag@gmail.com', 'Roberto A. Heredia');
-            $message->subject('Se venden Split, Lavadora, Cocina y Lijadoras.');
+            $message->subject('Se venden Split, Lavadora, Cocina y Lijadoras en la Habana');
         });
         \Log::info('Email electrodomesticos was sent.');
+        
+        \Mail::send('emails.sellTemplates.reello', [], function ($message) {
+            $message->to('comercioonline@googlegroups.com', 'comercioonline');
+            $message->to('las-avispas@googlegroups.com', 'las-avispas');
+            // $message->to('raherediag@gmail.com', 'Roberto A. Heredia');
+            $message->subject('Nuevo servicio para casas de alquiler.');
+        });
+        \Log::info('Email Reello was sent.');
         
         $this->info('Email was sent.');
     }

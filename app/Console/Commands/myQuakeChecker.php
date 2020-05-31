939f1fa25b946c9dc88bd4b84968b1abc66c0c80
Roberto A. Heredia Gonzalez <raherediag@gmail.com>
2020-05-31 12:51:19 -0400

    Cenais api updated.

--- app/Console/Commands/myQuakeChecker.php
+++ app/Console/Commands/myQuakeChecker.php
@@ -2,10 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Classes\misc\helper_class as cHelper;
 use Illuminate\Console\Command;
 use App\Models\quakes\quakes as mQuake;
 use DateTime;
 use DateTimeZone;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class myQuakeChecker extends Command
 {
@@ -32,7 +35,7 @@ class myQuakeChecker extends Command
     public function handle()
     {
         echo "Start Earthquake Checker";
-        \Log::info('Start Earthquake Checker');
+        Log::info('Start Earthquake Checker');
 
         $this->getCenaisInfo();
 
@@ -41,12 +44,12 @@ class myQuakeChecker extends Command
         $this->sendEmail();
 
         echo "End Earthquake Checker";
-        \Log::info('End Earthquake Checker');
+        Log::info('End Earthquake Checker');
     }
 
     private function getCenaisInfo()
     {
-        $cenais = ['latest', 'one', 'two', 'three'];
+        
         $last_quake = head(mQuake::where('provider', 'cenais')->orderby('date', 'DESC')->take(1)->get()->toArray());
 
         if ($last_quake == false) {
@@ -55,48 +58,46 @@ class myQuakeChecker extends Command
             $last_quake['time'] = strtotime($last_quake['date']);
         }
         $rows = [];
-        $adv = [];
-        foreach ($cenais as $query) {
-            $this->info('Receive information from Cenais with ' . $query . ' parameter');
-            $res = json_decode(\helper::apiCall('http://www.cenais.cu/lastquake/php/service.php', ['service' => $query], 'GET'), true);
+        
+        $this->info('Receive information from Cenais');
+        $res = json_decode(cHelper::apiCall('http://www.cenais.cu/lastquake/php/lastweek.php', [], 'GET'), true);
 
-            if (!is_array($res)) {
+        if (!is_array($res)) {
+            continue;
+        }
+
+        $this->info('Processing information');
+        foreach ($res as $quake) {
+            if (cHelper::getValue($quake, 'longitud', '') == '' && cHelper::getValue($quake, 'latitud', '') == '') {
                 continue;
             }
 
-            $this->info('Processing information');
-            foreach ($res as $quake) {
-                if (\helper::getValue($quake, 'lon', '') == '' && \helper::getValue($quake, 'lat', '') == '') {
-                    continue;
-                }
+            if (cHelper::getValue($quake, 'tiempoutc', '') != '') {
+                $quake['tiempoutc'] = str_replace(['/', '_'], ['-', ' '], $quake['tiempoutc']);
+                $quake['time'] = strtotime($quake['tiempoutc']);
+            } else {
+                $quake['time'] = strtotime('now');
+            }
 
-                if (\helper::getValue($quake, 'timeUTC', '') != '') {
-                    $quake['timeUTC'] = str_replace(['/', '_'], ['-', ' '], $quake['timeUTC']);
-                    $quake['time'] = strtotime($quake['timeUTC']);
-                } else {
-                    $quake['time'] = strtotime('now');
-                }
-
-                if ($quake['time'] > $last_quake['time']) {
-                    $rows[] = [
-                        'lng' => \helper::getValue($quake, 'lon', '0.00'),
-                        'lat' => \helper::getValue($quake, 'lat', '0.00'),
-                        'depth' => \helper::getValue($quake, 'z', '0'),
-                        'mag' => \helper::getValue($quake, 'magnitud', '0'),
-                        'provider' => \helper::getValue($quake, 'provider', 'cenais'),
-                        'description' => \helper::getValue($quake, 'desc', ''),
-                        'date' => \helper::getValue($quake, 'timeUTC', date('Y-m-d H:i:s')),
-                        'created_at' => date('Y-m-d H:i:s'),
-                        'updated_at' => date('Y-m-d H:i:s')
-                    ];
-                }
+            if ($quake['time'] > $last_quake['time']) {
+                $rows[] = [
+                    'lng' => cHelper::getValue($quake, 'longitud', '0.00'),
+                    'lat' => cHelper::getValue($quake, 'latitud', '0.00'),
+                    'depth' => cHelper::getValue($quake, 'profundidad', '0'),
+                    'mag' => cHelper::getValue($quake, 'magnitud', '0'),
+                    'provider' => cHelper::getValue($quake, 'provider', 'cenais'),
+                    'description' => cHelper::getValue($quake, 'distancialocalidad', '0') . 'Km ' . cHelper::getValue($quake, 'orientacion', '') . ' ' . cHelper::getValue($quake, 'nombre', '') . ', ' . cHelper::getValue($quake, 'provincia', ''),
+                    'date' => cHelper::getValue($quake, 'tiempoutc', date('Y-m-d H:i:s')),
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ];
             }
         }
+        
         if (count($rows) > 0) {
             $this->info('Saving information');
             mQuake::insert($rows);
         }
-
     }
 
     private function getUSGSInfo()
@@ -113,20 +114,20 @@ class myQuakeChecker extends Command
         $now = date('Y-m-d H:i:s');
         foreach ($usgs as $query) {
             $this->info('Receive information USGS provider of the last ' . $query . ' parameter');
-            $res = json_decode(\helper::apiCall('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_' .  $query . '.geojson', [], 'GET'), true);
+            $res = json_decode(cHelper::apiCall('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_' .  $query . '.geojson', [], 'GET'), true);
 
             if (!is_array($res)) {
                 continue;
             }
-            
+
             $this->info('Processing information');
             $features = $res['features'];
             foreach ($features as $quake) {
-                if (\helper::getValue($quake, 'type', '') != 'Feature') {
+                if (cHelper::getValue($quake, 'type', '') != 'Feature') {
                     continue;
                 }
 
-                if (\helper::getValue($quake, 'timeUTC', '') != '') {
+                if (cHelper::getValue($quake, 'timeUTC', '') != '') {
                     $quake['timeUTC'] = str_replace(['/', '_'], ['-', ' '], $quake['timeUTC']);
                     $quake['time'] = strtotime($quake['timeUTC']);
                 } else {
@@ -134,33 +135,33 @@ class myQuakeChecker extends Command
                 }
 
                 if ($quake['time'] > $last_quake['time']) {
-                    $time = date('c', \helper::getValue($quake, 'properties.time', microtime())/1000);
-                    $_id = \helper::getValue($quake, 'id', null);
+                    $time = date('c', cHelper::getValue($quake, 'properties.time', microtime()) / 1000);
+                    $_id = cHelper::getValue($quake, 'id', null);
                     $_quake = mQuake::where([
                         ['provider', 'usgs'],
                         ['provider_id', $_id],
                     ])->first();
                     $date = new DateTime($time);
                     $date->setTimezone(new DateTimeZone("UTC"));
-                    if($_quake){
-                        $_quake->lng = \helper::getValue($quake, 'geometry.coordinates.0', '0.00');
-                        $_quake->lat = \helper::getValue($quake, 'geometry.coordinates.1', '0.00');
-                        $_quake->depth = \helper::getValue($quake, 'geometry.coordinates.2', '0.00');
-                        $_quake->mag = \helper::getValue($quake, 'properties.mag', '0');
-                        $_quake->description = \helper::getValue($quake, 'properties.place', '0');
+                    if ($_quake) {
+                        $_quake->lng = cHelper::getValue($quake, 'geometry.coordinates.0', '0.00');
+                        $_quake->lat = cHelper::getValue($quake, 'geometry.coordinates.1', '0.00');
+                        $_quake->depth = cHelper::getValue($quake, 'geometry.coordinates.2', '0.00');
+                        $_quake->mag = cHelper::getValue($quake, 'properties.mag', '0');
+                        $_quake->description = cHelper::getValue($quake, 'properties.place', '0');
                         $_quake->date = $date->format("Y-m-d H:i:s");
                         $_quake->updated_at = $now;
                         $_quake->save();
-                    }else{
-                        if($_id){
+                    } else {
+                        if ($_id) {
                             $rows[$_id] = [
-                                'lng' => \helper::getValue($quake, 'geometry.coordinates.0', '0.00'),
-                                'lat' => \helper::getValue($quake, 'geometry.coordinates.1', '0.00'),
-                                'depth' => \helper::getValue($quake, 'geometry.coordinates.2', '0'),
-                                'mag' => \helper::getValue($quake, 'properties.mag', '0'),
-                                'provider' => \helper::getValue($quake, 'provider', 'usgs'),
+                                'lng' => cHelper::getValue($quake, 'geometry.coordinates.0', '0.00'),
+                                'lat' => cHelper::getValue($quake, 'geometry.coordinates.1', '0.00'),
+                                'depth' => cHelper::getValue($quake, 'geometry.coordinates.2', '0'),
+                                'mag' => cHelper::getValue($quake, 'properties.mag', '0'),
+                                'provider' => cHelper::getValue($quake, 'provider', 'usgs'),
                                 'provider_id' => $_id,
-                                'description' => \helper::getValue($quake, 'properties.place', ''),
+                                'description' => cHelper::getValue($quake, 'properties.place', ''),
                                 'date' => $date->format("Y-m-d H:i:s"),
                                 'created_at' => $now,
                                 'updated_at' => $now
@@ -174,10 +175,10 @@ class myQuakeChecker extends Command
             $this->info('Saving information');
             mQuake::insert($rows);
         }
-        
     }
 
-    private function sendEmail(){
+    private function sendEmail()
+    {
 
         $features = mQuake::where([
             ['mag', '>', 3],
@@ -186,12 +187,12 @@ class myQuakeChecker extends Command
             ['lat', '>', 19],
             ['lat', '<', 21],
             ['sent', '=', 0],
-        ])->orderBy('date','DESC')->get()->toArray();
+        ])->orderBy('date', 'DESC')->get()->toArray();
 
         if (count($features) > 0) {
-            \Mail::send('emails.quake_warning', ['contents' => ['quakes' => $features]], function ($message) {
+            Mail::send('emails.quake_warning', ['contents' => ['quakes' => $features]], function ($message) {
                 $message->to('raherediag@gmail.com', 'Roberto A. Heredia');
-                $message->to('tfuentes@gmail.com', 'Teresita Fuentes');
+                $message->to('tfuentes1307@gmail.com', 'Teresita Fuentes');
                 $message->subject('Quake warning');
             });
             $this->info('Email was sent.');
@@ -200,7 +201,7 @@ class myQuakeChecker extends Command
                 $adv[] = $quake['id'];
             }
 
-            $features = mQuake::whereIn('id',$adv)->update(['sent' => 1]);
+            $features = mQuake::whereIn('id', $adv)->update(['sent' => 1]);
         }
     }
 }
